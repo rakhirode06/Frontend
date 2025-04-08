9a0ad19757952683768dcaf20b649e65e5bc115b
rakhirode06 <2022bit006@sggs.ac.in>
2025-04-08 17:43:41 +0530

    first commit

--- TY_IT_8Sept/settings.py
+++ TY_IT_8Sept/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-import os
+CSRF_TRUSTED_ORIGINS=["web-production-9bf8.up.railway.app"]
 
 
 # Quick-start development settings - unsuitable for production
